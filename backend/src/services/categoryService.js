118b94a03c2dd3ee3e5b8a70446212b138352d54
johan101423 <joelhernava@gmail.com>
2026-07-21 18:14:23 -0500

    Add category service with CRUD methods
    Implement category service functions for CRUD operations

---
+++ backend/src/services/categoryService.js
@@ -0,0 +1,21 @@
+export const categoryRepository from '../repositories/categoryRepository.js';
+
+const getAll = () => {
+    return categoryRepository.findAll();
+};
+
+const getById = (id) => {
+    return categoryRepository.findById(id);
+};
+
+const createNew = (data) => {
+    return categoryRepository.create(data);
+};
+
+const updateExisting = (id, data) => {
+    return categoryRepository.update(id, data);
+};
+
+const remove = (id) => {
+    return categoryRepository.delete(id);
+};
